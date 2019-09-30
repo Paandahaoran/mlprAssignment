2b5dcaad3916f92846f04cb77b6a803d8c2b0464
Paanda <pandapanhaoran@gmail.com>
2019-09-30 12:23:14 +0100

    Update Q2.py

--- as_1audio/Q2.py
+++ as_1audio/Q2.py
@@ -2,8 +2,8 @@ import numpy as np
 import  matplotlib.pyplot as plt
 import data_preprocess
 t = np.arange(0,1,0.05)
-y = np.array(data_preprocess.X_shuf_train[0].flatten())
-target = np.array(data_preprocess.y_shuf_train[0].flatten())
+y = np.array(data_preprocess.X_shuf_train[int(np.random()*100)].flatten())
+target = np.array(data_preprocess.y_shuf_train[77].flatten())
 
 
 
@@ -20,7 +20,7 @@ def phi_quadratic(Xorig):
 def fit_and_plot(phi_fn,X,yy):
     w_fit = np.linalg.lstsq(phi_fn(X),yy)[0]
     #w_fit=np.dot(np.linalg.pinv(phi_fn(X)),yy)
-    x_grid = np.arange(0,1.2,0.01)
+    x_grid = np.arange(0,1,0.01)
     f_grid = np.dot(phi_fn(x_grid),w_fit)
     plt.plot(x_grid,f_grid,lw=2)
 
@@ -29,6 +29,6 @@ def fit_and_plot(phi_fn,X,yy):
 plt.plot(t,y[0],'bD')
 fit_and_plot(phi_linear,t,y[0])
 fit_and_plot(phi_quadratic,t,y[0])
-plt.plot(1,target[0],'rD')
+plt.plot(1,target,'rD')
 plt.legend(['traning points','line fit','quadratic fit','test point'],loc='lower left')
 plt.show()
